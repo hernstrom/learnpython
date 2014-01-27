cf6187a5b5f8c4a8f1353c442879ce10e01c1404
hernstrom <hernstrom@gmail.com>
2014-01-26 20:36:43 -0600

    Ex 11 complete

---
+++ ex11/ex11.py
@@ -0,0 +1,12 @@
+#print "How old are you?",
+#age = raw_input()
+age = raw_input("How old are you? ")
+#print "How tall are you?",
+#height = raw_input()
+height = raw_input("How tall are you? ")
+#print "How much do you weight?",
+#weight = raw_input()
+weight = raw_input("How much do you weight? ")\
+
+print "So, you're %s old, %s tall, and %s heavy." % (
+    age, height, weight)
