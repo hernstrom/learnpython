5ee1640d96b84adb29a2864fe2a988df86905e5a
hernstrom <hernstrom@gmail.com>
2014-02-02 13:25:44 -0600

    Adding latest completed exercies

---
+++ ex13/ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second, third = argv
+
+fourth = raw_input("You forgot one: ")
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+print "Your fourth variable is:", fourth
+
